c237af69dae96f18fc35518e8a8c6b30b51394da
Julia Marin Salas <jul.ms1193@gmail.com>
2024-11-24 16:03:22 +0100

    Ejercicios del 1 al 17

---
+++ Ejercicio técnico módulo 2 - Julia.sql
@@ -0,0 +1,189 @@
+-- EVALUACIÓN FINAL MÓDULO 2 --    -- Por Julia Marín Salas
+
+USE sakila;
+
+-- Ejercicio 1: Selecciona todos los nombres de las películas sin que aparezcan duplicados.
+
+SELECT 
+DISTINCT(title)
+FROM film;
+
+-- Ejercicio 2: Muestra los nombres de todas las películas que tengan una clasificación de "PG-13".
+
+SELECT 
+title,
+rating
+FROM film
+WHERE rating = 'PG-13';
+
+-- Ejercicio 3: Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
+
+SELECT 
+title, 
+description
+FROM film
+WHERE description LIKE '%amazing%';
+
+-- Ejercicio 4: Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos.
+
+SELECT 
+title,
+length
+FROM film
+WHERE length > 120;
+
+-- Ejercicio 5: Recupera los nombres de todos los actores.
+
+SELECT DISTINCT 
+first_name
+FROM actor
+GROUP BY 
+first_name,
+last_name
+ORDER BY
+first_name;
+
+-- Ejercicio 6: Encuentra el nombre y apellido de los actores que tengan "Gibson" en su apellido.
+
+SELECT
+first_name AS Nombre,
+last_name AS Apellido
+FROM actor
+WHERE last_name = 'Gibson';
+
+-- Ejercicio 7: Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20.
+
+SELECT 
+first_name AS Nombre
+FROM actor 
+WHERE actor_id BETWEEN 10 AND 20;
+
+-- Ejercicio 8: Encuentra el título de las películas en la tabla film que no sean ni "R" ni "PG-13" en cuanto a su clasificación.
+
+SELECT 
+title,
+rating
+FROM film
+WHERE rating NOT IN ('R', 'PG-13');
+
+-- Ejercicio 9: Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación 
+-- junto con el recuento.
+
+SELECT
+rating AS Clasificación,
+COUNT(film_id) AS NúmeroPeliculas
+FROM film
+GROUP BY 
+Clasificación;
+
+-- Ejercicio 10: Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su
+-- nombre y apellido junto con la cantidad de películas alquiladas.
+
+SELECT 
+c.customer_id AS ID_Cliente,
+c.first_name AS Nombre,
+c.last_name AS Apellido,
+COUNT(r.rental_id) AS Cantidad_Peliculas_Alquiladas
+FROM 
+customer c
+LEFT JOIN 
+rental r ON c.customer_id = r.customer_id
+GROUP BY 
+c.customer_id, c.first_name, c.last_name
+ORDER BY 
+Cantidad_Peliculas_Alquiladas DESC;
+
+-- Ejercicio 11:  Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría
+-- junto con el recuento de alquileres.
+
+
+SELECT 
+category.name AS Categoria,
+COUNT(rental.rental_id) AS Total_Alquileres
+FROM 
+category
+INNER JOIN film_category 
+ON category.category_id = film_category.category_id
+INNER JOIN inventory  
+ON film_category.film_id = inventory.film_id
+INNER JOIN rental 
+ON inventory.inventory_id = rental.inventory_id
+GROUP BY 
+category.name
+ORDER BY 
+Total_Alquileres DESC;
+
+-- Ejercicio 12: Encuentra el promedio de duración de las películas para cada clasificación de la tabla film y muestra la
+-- clasificación junto con el promedio de duración.
+
+SELECT
+rating AS clasificación,
+AVG(length) AS Promedio_duración
+FROM film
+GROUP BY
+clasificación
+ORDER BY
+clasificación;
+
+-- Ejercicio 13: Encuentra el nombre y apellido de los actores que aparecen en la película con title "Indian Love".
+
+SELECT 
+first_name AS NombreActor,
+last_name AS ApellidoActor
+FROM 
+actor 
+INNER JOIN 
+film_actor ON actor.actor_id = film_actor.actor_id
+INNER JOIN 
+film ON film_actor.film_id = film.film_id
+WHERE 
+film.title = 'Indian Love';
+
+-- Ejercicio 14: Muestra el título de todas las películas que contengan la palabra "dog" o "cat" en su descripción.
+
+SELECT 
+title,
+description
+FROM film
+WHERE description LIKE '%dog%' OR '%cat%';
+
+-- Ejercicio 15: Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010.
+
+SELECT 
+title,
+release_year
+FROM film
+WHERE release_year BETWEEN 2005 AND 2010
+ORDER BY 
+release_year ASC;
+
+-- Ejercicio 16:  Encuentra el título de todas las películas que son de la misma categoría que "Family".
+
+SELECT 
+film.title AS Titulo
+FROM 
+film 
+INNER JOIN 
+film_category 
+ON film.film_id = film_category.film_id
+INNER JOIN 
+category  
+ON film_category.category_id = category.category_id
+WHERE 
+category.name = 'Family';
+
+-- Ejercicio 17: Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla film.
+
+SELECT
+film.title AS Titulo,
+rating AS Categoria,
+length AS Duración
+FROM
+film
+WHERE rating = 'R' AND length > 120 
+ORDER BY
+Duración ASC
+;
+
+-- BONUS --
+
